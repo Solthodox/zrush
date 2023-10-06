fc5c2683a6ef782f13404899fa6f000f36855117
Solthodox <mrwojack@proton.me>
2023-10-06 11:08:52 +0200

    allow non-snake-case

--- src/utils/ethers_empty_types.rs
+++ src/utils/ethers_empty_types.rs
@@ -1,13 +1,16 @@
 use ethers::types::{Address, Signature, U256};
 
+#[allow(non_snake_case)]
 pub fn U256_ZERO() -> U256 {
     U256::from(0)
 }
 
+#[allow(non_snake_case)]
 pub fn ADDRESS_ZERO() -> Address {
     Address::from([0u8; 20])
 }
 
+#[allow(non_snake_case)]
 pub fn EMPTY_SIGNATURE() -> Signature {
     Signature {
         r: U256_ZERO(),
